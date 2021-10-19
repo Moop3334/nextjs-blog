a600401b638437e8a50c18b703497bc3fd362077
Moop3334 <Moop3334@gmail.com>
2021-10-19 17:09:30 -0600

    Changed Website Head [up2]

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'STS Robotics'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Robot Fight Club'
 
 export default function Layout({ children, home }) {
   return (
@@ -15,7 +15,7 @@ export default function Layout({ children, home }) {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="Learn how to build a personal website using Next.js"
+            content="STS Blog Website"
           />
           <meta
             property="og:image"
