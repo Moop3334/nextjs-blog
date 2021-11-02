e17d3ccfa424857d20c50fbc0bf7282f52e12702
Moop3334 <Moop3334@gmail.com>
2021-11-02 17:12:32 -0600

    Added new page [up3]

---
+++ pages/about.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import styles from '.layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+export default function About({ allPostsData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>About Us:</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          We are STS Robot Fight Club, a FIRST Robotics team based out of Strathcona-Tweedsmir School. Our team was established during the 2020-2021 school year, and while we originally only had one team, we expanded our ranks this year to include a Grade 10 and Grade 11 Team. We will probably expand our numbers next year as well to include a Grade 12 team as well.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Our Team:</h2>
+      </section>
+    </Layout>
+  )
+}
