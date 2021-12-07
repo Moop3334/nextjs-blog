d38b37530a52c881876dbc946b3d081e0a357312
Moop3334 <79484915+Moop3334@users.noreply.github.com>
2021-12-07 16:00:32 -0700

    Updated layout.js [up3]

--- components/layout.js
+++ components/layout.js
@@ -56,8 +56,8 @@ export default function Layout({ children, home }) {
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
-                  height={124}
-                  width={124}
+                  height={144}
+                  width={144}
                   alt={name}
                 />
                 <h1 className={utilStyles.heading2Xl}>{name}</h1>
